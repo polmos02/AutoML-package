f7172c1883660521c5a7ade1d37ae72232f4a338
klaudia <kwokaklaudia2002@gmail.com>
2025-01-13 14:11:06 +0100

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="AutoMushroom",
     version="0.0.0",
-    packages=find_packages(),
     description="A Python package for classification of mushrooms",
     url="https://github.com/polmos02/AutoML-package/tree/main",
     classifiers=[
@@ -11,4 +10,11 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.11',
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.2.2",
+        "scikit-learn>=1.5.1",
+        "seaborn>=0.13.2",
+        "matplotlib>=3.9.2",
+    ]
 )
